06590e424e5177b5b67aa27717bfb6c1a43ee4c7
topp13 <topp13@mail.ru>
2020-12-10 13:33:41 +0300

    Another try

--- join_server.cpp
+++ join_server.cpp
@@ -41,7 +41,26 @@ private:
                                         split_vector_type commands; // #2: Search for tokens
                                         split(commands, str_data, boost::algorithm::is_any_of("\n") );
 
-
+                                        commands =
+                                                {
+                                                "INSERT A 0 lean",
+                                                "INSERT A 0 understand",
+                                                "INSERT A 1 sweater",
+                                                "INSERT A 2 frank",
+                                                "INSERT A 3 violation",
+                                                "INSERT A 4 quality",
+                                                "INSERT A 5 precision",
+                                                "INSERT B 3 proposal",
+                                                "INSERT B 4 example",
+                                                "INSERT B 5 lake",
+                                                "INSERT B 6 flour",
+                                                "INSERT B 7 wonder",
+                                                "INSERT B 8 selection",
+                                                "INTERSECTION",
+                                                "SYMMETRIC_DIFFERENCE",
+                                                "TRUNCATE A",
+                                                "TRUNCATE B",
+                                                };
 
                                         const boost::regex insert_filter("INSERT [AB] [0-9]+ [a-zA-Z]+");
                                         const boost::regex trucate_filter("TRUNCATE [AB]");
